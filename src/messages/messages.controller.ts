250648eb83022370b80143c93865ef67c12ae536
Kudi User <kudiuser@Kudis-MacBook-Pro.local>
2023-11-29 22:21:11 +0100

    first commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,33 @@
+import { Controller , Get, Post, Body, Param, NotFoundException} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message-dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+    messagesService: MessagesService;
+
+    constructor(){
+        this.messagesService = new MessagesService()
+    }
+    
+    @Get()
+    listMessages() {
+       return this.messagesService.findAll()
+    }
+
+    @Post()
+    createMessage(@Body() body: CreateMessageDto) {
+        return this.messagesService.create(body.content)
+    }
+
+    @Get("/:id")
+    async getMessageByID(@Param("id") id: string) {
+       const message = await this.messagesService.findOne(id)
+
+       if(!message) {
+            throw new NotFoundException("message not found")
+       }
+
+       return message
+    }
+}
